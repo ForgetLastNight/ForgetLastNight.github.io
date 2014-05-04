09b0ee02a677a3d2e44bea181a698af6e52e1d1c
Henry Spindell <henryspindell@gmail.com>
2014-05-04 14:16:15 -0500

    initialize homepage

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+document.write("</div>"+
+ 
+    "<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js'></script>"+
+  
+  "</body>"+
+
+"</html>");
---
+++ js/header.js
@@ -0,0 +1,26 @@
+document.write(
+    "<title>Forget Last Night</title>"+
+    "<meta charset='utf-8'>"+
+    "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"+
+    "<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'>"+
+    "<meta name='description' content='>"+
+    "<meta name='Team Orange' content='>"+
+    "<meta name='apple-mobile-web-app-capable' content='yes'>"+
+    "<meta name='apple-mobile-web-app-status-bar-style' content='black-translucent'>"+
+
+    "<link href='css/bootstrap.min.css' rel='stylesheet'>"+
+    "<link href='css/style.css' rel='stylesheet'>"+
+
+    "<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js'></script>"+
+    "<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.0/fastclick.min.js'></script>"+
+  "</head>"+
+  
+  "<body>"+
+    "<div class='navbar navbar-fixed-top' role='navigation'>"+
+      "<div class='container'>"+
+            "<center style='vertical-align:middle;'><h3>Forget Last Night</h3></center>"+
+      "</div>"+
+    "</div>"+
+
+    "<div class='container' id='main'>"
+   );
