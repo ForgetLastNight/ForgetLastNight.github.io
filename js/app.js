cc0ec47b056d7ac54d56c8e85ecc1d667f2d7472
arundhatijaswal <arundhatijaswal2013@u.northwestern.edu>
2014-05-05 20:26:12 -0500

    Merge branch 'master'
    Conflicts:
    	js/app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 	
-
-
-
+	Parse.initialize('JzA8q6sistqJipqlSGVoQjgzUIkM5DTT4TiAmun9', 'ME6ADe5O6EAxaznIONYLpECysNoE9TzRWi3F8BtK');
+	Parse.Cloud.run('hello', {}, {
+	  success: function(result) {
+	    // result is 'Hello world!'
+	    console.log(result);
+	  },
+	  error: function(error) {
+	  }
+	});
 
 });
