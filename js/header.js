99aad55904d12cdcf7477beb48af1386369757bc
Henry Spindell <henryspindell@gmail.com>
2014-05-05 20:01:39 -0500

    Linked to Parse

--- js/header.js
+++ js/header.js
@@ -11,6 +11,7 @@ document.write(
     "<link href='css/bootstrap.min.css' rel='stylesheet'>"+
     "<link href='css/style.css' rel='stylesheet'>"+
 
+    "<script src='https://www.parsecdn.com/js/parse-1.2.18.min.js'></script>"+
     "<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js'></script>"+
     "<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.0/fastclick.min.js'></script>"+
   "</head>"+
