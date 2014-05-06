93df6d574cffad47b27fcd5710655c9875b2addd
Henry Spindell <henryspindell@gmail.com>
2014-05-05 20:51:44 -0500

    fix

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 	Parse.initialize('JzA8q6sistqJipqlSGVoQjgzUIkM5DTT4TiAmun9', 'ME6ADe5O6EAxaznIONYLpECysNoE9TzRWi3F8BtK');
       
 
-
 	Parse.Cloud.run('hello', {}, {
 	  success: function(result) {
 	    console.log(result);
