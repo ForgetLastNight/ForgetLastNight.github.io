b79ef7a2b65f1e7d837fbbe5512aeb32a76e15d4
Henry Spindell <henryspindell@gmail.com>
2014-05-06 23:14:41 -0500

    style stufF

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -39,20 +39,61 @@ Parse.Cloud.define("ShareToTwitter", function(request, response){
     var encodedSig=oauth.percentEncode(sig); //finally you got oauth signature
     console.log("Encoded Signature: "+encodedSig);
 
- Parse.Cloud.httpRequest({
-    method: 'POST',
-    url: urlLink,
-    headers: {
-        "Authorization": 'OAuth oauth_consumer_key='+oauth_consumer_key+', oauth_nonce='+nonce+', oauth_signature='+encodedSig+', oauth_signature_method="HMAC-SHA1", oauth_timestamp='+timestamp+',oauth_token='+oauth_token+', oauth_version="1.0"'
-   },
-   body: {
-     "status" : postSummary,      
-   },
-   success: function(httpResponse) {
-     response.success(httpResponse.text);
-   },
-   error: function(httpResponse) {
-        response.error('Request failed with response' + httpResponse.status + "and the tweet is "+request.params.tweet);
-   }
- });
+    Parse.Cloud.httpRequest({
+        method: 'POST',
+        url: urlLink,
+        headers: {
+            "Authorization": 'OAuth oauth_consumer_key='+oauth_consumer_key+', oauth_nonce='+nonce+', oauth_signature='+encodedSig+', oauth_signature_method="HMAC-SHA1", oauth_timestamp='+timestamp+',oauth_token='+oauth_token+', oauth_version="1.0"'
+        },
+        body: {
+         "status" : postSummary,      
+        },
+        success: function(httpResponse) {
+         response.success(httpResponse.text);
+        },
+        error: function(httpResponse) {
+            response.error('Request failed with response' + httpResponse.status + "and the tweet is "+request.params.tweet);
+        }
+    });
+});
+
+
+Parse.Cloud.define("GetFromTwitter", function(request, response){
+    var urlLink = 'https://api.twitter.com/1.1/search/tweets.json';
+    var query = oauth.percentEncode(request.params.searchQuery);
+    var nonce = oauth.nonce(32);
+    var ts = Math.floor(new Date().getTime() / 1000);
+    var timestamp = ts.toString();
+    var oauth_token = "2477479568-baSd4MdKrVH61jtmywgxympwqYO2b17jIiAWY70";
+    var tokenSecret = "9rYhMNnBZNHNUdKnzEB6yLy5LCBEPTryfGvdpIYkd6ncI";
+    var accessor = {"consumerSecret" : "N4BpOKkuOvP2vMbGnw6jXjCbgdqwghblQglryFigbN3Y89dWSY", "tokenSecret": tokenSecret};
+    var message = {"method" : "GET", "action": urlLink, "parameters":oauth.decodeForm("q="+query)};
+    message.parameters.push(["oauth_version","1.0"]);
+    message.parameters.push(["oauth_consumer_key", "6QMz8aPAbzNw3XrLwTmaWaOMW"]);
+    message.parameters.push(["oauth_token", oauth_token]);
+    message.parameters.push(["oauth_timestamp",timestamp]);
+    message.parameters.push(["oauth_nonce", nonce]);
+    message.parameters.push(["oauth_signature_method", "HMAC-SHA1"]);
+    oauth.SignatureMethod.sign(message, accessor);
+    var normPar = oauth.SignatureMethod.normalizeParameters(message.parameters);
+    var baseString = oauth.SignatureMethod.getBaseString(message);
+    var sig = oauth.getParameter(message.parameters, "oauth_signature")+"=";
+    var encodedSig=oauth.percentEncode(sig);
+
+     Parse.Cloud.httpRequest({
+       method: 'GET',
+       url: urlLink,
+       headers: {
+        "Authorization": 'OAuth oauth_consumer_key="6QMz8aPAbzNw3XrLwTmaWaOMW", oauth_nonce='+nonce+', oauth_signature='+encodedSig+', oauth_signature_method="HMAC-SHA1", oauth_timestamp='+timestamp+',oauth_token='+oauth_token+', oauth_version="1.0"'
+      },
+      body: {
+         "q" : request.params.searchQuery,     
+      },
+      success: function(httpResponse) {
+         response.success(httpResponse.text);
+      },
+      error: function(httpResponse) {
+        response.error(httpResponse.text);
+      }
+    });
 });
--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
-	
-	$('#forget').click(function(){
 
-		message = prompt("Enter tweet here");
 
-		Parse.Cloud.run('ShareToTwitter',{tweet: message}, {
+		// message = prompt("Enter tweet here");
+
+		// Parse.Cloud.run('ShareToTwitter',{tweet: message}, {
+		//   success: function(result) {
+		//     console.log("Tweet posted to twitter!");
+		//   },
+		//   error: function(error) {
+		//   }
+		// });
+
+
+
+
+	$('#get').click(function(){
+
+		var s ="#mtv vine.co filter:links since:2011-11-11";
+		Parse.Cloud.run('GetFromTwitter',{searchQuery : s}, {
 		  success: function(result) {
-		    alert("Success!");
+		  	console.log("Tweets retrieved from twitter!");
 		  },
 		  error: function(error) {
 		  }
 		});
 
 	});
-
 
 	
 
