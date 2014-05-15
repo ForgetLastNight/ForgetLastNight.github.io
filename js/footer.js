8f10a22c714a679156ada547cc0a0dca60b5d362
arundhatijaswal <arundhatijaswal2013@u.northwestern.edu>
2014-05-14 22:49:55 -0500

    minor footer style update

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ document.write("</div>"+
  
     "<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js'></script>"+
 
-    "<a style='position:absolute;bottom:0;width:100%;' href='http://www.privacychoice.org/policy/text?policy=9f01ac40f3360f751f2641305d5b797c'>Privacy Policy</a>"+
+    "<a style='position:absolute;bottom:0;width:100%;margin-left:auto;margin-right:auto;color:white;' href='http://www.privacychoice.org/policy/text?policy=9f01ac40f3360f751f2641305d5b797c'>Privacy Policy</a>"+
   
   "</body>"+
 
