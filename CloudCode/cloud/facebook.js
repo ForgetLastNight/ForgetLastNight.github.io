9e993e0b5dc7d5803212eb114687a639e56d51d9
brian-0114 <limit56@gmail.com>
2014-05-18 15:32:19 -0500

    facebook displaying

--- CloudCode/cloud/facebook.js
+++ CloudCode/cloud/facebook.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
+      console.log(JSON.stringify(response));
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
     });
@@ -81,6 +81,7 @@ $(document).ready(function(){
       'me/news.reads',
       'get',
       function(response) {
+      console.log("successful");
       console.log(response);    // handle the response
   }
 );
