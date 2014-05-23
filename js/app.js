00be03d15e0027ae005c7f16f89f2a526b9035e8
brian-0114 <limit56@gmail.com>
2014-05-22 23:43:37 -0500

    finishing fb integration; adding fb icon

--- js/app.js
+++ js/app.js
@@ -313,7 +313,7 @@ function testAPI() {
                title = "no title";
                id = response['data'][i]['id'];
                message = response['data'][i]['message'];
-               var FBHTML = "<div class='row' ><div class='col-xs-2 logo'><img class='logo_tw' src='tumblr-logo.png'/></div><div class='col-xs-9 message'><p><span class='time-tw'>"+time+"</span><br/>"+title+"<br/>"+message+"</p></div><div class='col-xs-1 delete-box delete-tumblr'><input type='checkbox' name='"+id+"'/></div></div>";
+               var FBHTML = "<div class='row' ><div class='col-xs-2 logo'><img class='logo_tw' src='facebook-icon.png'/></div><div class='col-xs-9 message'><p><span class='time-tw'>"+time+"</span><br/>"+title+"<br/>"+message+"</p></div><div class='col-xs-1 delete-box delete-tumblr'><input type='checkbox' name='"+id+"'/></div></div>";
                $('#display-media').append(FBHTML);
                   }
             }
