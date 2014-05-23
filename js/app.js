c80d14a8f817fe6785e3c3cd36b3d7348081d189
brian-0114 <limit56@gmail.com>
2014-05-22 22:38:21 -0500

    fb integration

--- js/app.js
+++ js/app.js
@@ -106,24 +106,11 @@ $(document).ready(function(){
   //       'into Facebook.';
   //   }
   // }
-FB.getLoginStatus(function(response) {
+FB.Event.subscribe('auth.authResponseChange', function(response) {
   if (response.status === 'connected') {
-    // the user is logged in and has authenticated your
-    // app, and response.authResponse supplies
-    // the user's ID, a valid access token, a signed
-    // request, and the time the access token 
-    // and signed request each expire
-    var uid = response.authResponse.userID;
-    var accessToken = response.authResponse.accessToken;
-    console.log(uid,accessToken);
-    //testAPI();
-  } else if (response.status === 'not_authorized') {
-    // the user is logged in to Facebook, 
-    // but has not authenticated your app
-  } else {
-    // the user isn't logged in to Facebook.
+    console.log("connected");
   }
- });
+});
 
 //   function testAPI() {
 
