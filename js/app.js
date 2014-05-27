bee6095f447a83e5ff187f0d15586abb2407df98
brian-0114 <limit56@gmail.com>
2014-05-27 15:45:48 -0500

    fb fql

--- js/app.js
+++ js/app.js
@@ -122,30 +122,39 @@ $(document).ready(function(){
 		if(window.localStorage['fbIsSynced']=='yes')
 		{	
 			FB.api(
-				'me/activities',
-				'get',
 				{
-					access_token : fbToken,
+				method: 'fql.query',
+				query: 'SELECT name FROM user WHERE uid=me()'
 				},
-				function(response) {
-					if (!response || response.error) {
-					alert('There was an error connecting to Facebook.');
-					} 
-					else {
-						console.log(response);
-						// for (var i = 0 ; i< response.data.length;i++)
-						// {
-						// 	time = response['data'][i]['created_time'];
-						// 	title = "no title";
-						// 	id = response['data'][i]['id'];
-						// 	message = response['data'][i]['message'];
-						// 	var FBHTML = "<div class='row' ><div class='col-xs-2 logo'><img class='logo_tw' src='facebook-icon.png'/></div><div class='col-xs-9 message'><p><span class='time-tw'>"+time+"</span><br/>"+title+"<br/>"+message+"</p></div><div class='col-xs-1'></div></div>";
-						// 	$('#display-media').append(FBHTML);
-						// }
-					}
-
+			function(response) {
+				alert('Your name is ' + response[0].name);
 				}
 			);
+			// FB.api(
+			// 	'me/activities',
+			// 	'get',
+			// 	{
+			// 		access_token : fbToken,
+			// 	},
+			// 	function(response) {
+			// 		if (!response || response.error) {
+			// 		alert('There was an error connecting to Facebook.');
+			// 		} 
+			// 		else {
+			// 			console.log(response);
+			// 			// for (var i = 0 ; i< response.data.length;i++)
+			// 			// {
+			// 			// 	time = response['data'][i]['created_time'];
+			// 			// 	title = "no title";
+			// 			// 	id = response['data'][i]['id'];
+			// 			// 	message = response['data'][i]['message'];
+			// 			// 	var FBHTML = "<div class='row' ><div class='col-xs-2 logo'><img class='logo_tw' src='facebook-icon.png'/></div><div class='col-xs-9 message'><p><span class='time-tw'>"+time+"</span><br/>"+title+"<br/>"+message+"</p></div><div class='col-xs-1'></div></div>";
+			// 			// 	$('#display-media').append(FBHTML);
+			// 			// }
+			// 		}
+
+			// 	}
+			// );
 		}
 
 	});
