febaec47c6dad5430ca6bd3744f73a4678a443c8
brian-0114 <limit56@gmail.com>
2014-05-29 19:18:15 -0500

    fb time

--- js/app.js
+++ js/app.js
@@ -163,6 +163,21 @@ $(document).ready(function(){
 					} 
 					else {
 						console.log(response);
+						fb_posts = response['Object']['data'];
+						for(var i=0;i<fb_posts.length;i++)
+						{
+						var GMT_time =response[i]['Object']['created_time'];
+						var local_time_fb = new Date(GMT_time);
+
+						title=response[i]['Object']['title']?posts[i]['title']:"(No title)";						
+						message=response[i]['Object']['message'];
+						id = String(response[i]['Object']['id']);
+						var hours = $('#time-range').val(); 
+						if(fb_inrange(local_time_fb,hours)){
+						var fb_html = "<div class='row' ><div class='col-xs-2 logo'><img class='logo_tw' src='facebook-icon.png'/></div><div class='col-xs-9 message'><p><span class='time-tw'>"+Date(time)+"</span><br/><b>"+title+"</b><br/>"+message+"</p></div><div class='col-xs-1 delete-box delete-tumblr'><input type='checkbox' name='"+id+"'/></div></div>";
+									$('#display-media').append(fb_html);						
+								}
+						}
 
 					}
 
@@ -282,6 +297,19 @@ $(document).ready(function(){
 		//while loop to refresh once all async deletes are finished
 
 	});
+
+	function fb_inrange(fb_time,hours){
+		var currentTime  = new Date();
+		//console.log("now"+currentTime);		
+		var sec = currentTime.valueOf();
+		//console.log("tublr:"+time);
+		var post_time = new Date(fb_time);
+		//console.log("tublr:"+post_time);
+		var x_hours_beforetosec = sec - 1000*hours*60*60;
+		var post_time2sec=post_time.valueOf();
+		if( post_time2sec >= x_hours_beforetosec  ) return true;
+		else return false;
+	}
 
 	function timeRange(tumblr_time,hours){
 
