b1ec0a2c722d6c00af20e1e85f5015277cd13eaf
tyrowang <wangjianan87@gmail.com>
2014-06-01 22:20:21 -0500

    delete the alert info
    The user may not like to see it:)

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var client = new ZeroClipboard( document.getElementById("copy-button"), {
 } );
 
 client.on( "load", function(client) {
-  alert( "movie is loaded" );
+  //alert( "movie is loaded" );
 
   client.on( "complete", function(client, args) {
     // `this` is the element that was clicked
