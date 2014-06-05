b51f4bd01fdd4f7a3a3280c20e15d8a8283ba907
Henry Spindell <henryspindell@gmail.com>
2014-06-04 23:27:47 -0500

    font size

--- js/footer.js
+++ js/footer.js
@@ -2,8 +2,8 @@ document.write("</div>"+
  	
     "<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.1.1/js/bootstrap.min.js'></script>"+
 
-   "<div style='margin-left:5px;font-size:17px;float:left;'><a style='color:white;' id='privacy' href='signup.html'>Log Out</a></div>"+
-  "<div style='margin-right:5px;font-size:17px;float:right;'><a style='color:white;' id='privacy' href='http://www.privacychoice.org/policy/text?policy=9f01ac40f3360f751f2641305d5b797c'>Privacy Policy</a></div>"+
+   "<div style='margin-left:5px;font-size:15px;float:left;'><a style='color:white;' id='privacy' href='signup.html'>Log Out</a></div>"+
+  "<div style='margin-right:5px;font-size:15px;float:right;'><a style='color:white;' id='privacy' href='http://www.privacychoice.org/policy/text?policy=9f01ac40f3360f751f2641305d5b797c'>Privacy Policy</a></div>"+
   "</body>"+
 
 "</html>");
